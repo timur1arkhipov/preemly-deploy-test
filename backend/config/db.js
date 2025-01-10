801f27239ee86ba2ecf0b920a48e9c25eed34563
t.arhipov <t.arhipov@medpoint24.ru>
2025-01-10 19:54:11 +0400

    asdasds

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined");
+    }
 
-    console.log(`MongoDB Connected : ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
