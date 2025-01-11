cdab22584d398c7fdb6f7a3a96c8ebc0c92293bf
t.arhipov <t.arhipov@medpoint24.ru>
2025-01-11 13:41:26 +0400

    asd

--- backend/index.js
+++ backend/index.js
@@ -342,9 +342,9 @@ app.put("/api/guests/:id/attendance", async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
