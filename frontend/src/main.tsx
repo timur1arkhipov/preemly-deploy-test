68e3fcbfe61bb8e13c7c5c4006299d35c804790c
t.arhipov <t.arhipov@medpoint24.ru>
2025-01-12 00:48:46 +0400

    asd

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,26 +1,26 @@
-import React from "react"; // Add this line
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
-const domain = "dev-hiwm6xv136r7yzch.us.auth0.com";
-const clientId = "TEIKAz5dnBhTiOjRkNsjMHehoZtsC3Ku";
+  import React from "react"; // Add this line
+  import ReactDOM from "react-dom/client";
+  import "./index.css";
+  import App from "./App";
+  import { Auth0Provider } from "@auth0/auth0-react";
+  const domain = "dev-hiwm6xv136r7yzch.us.auth0.com";
+  const clientId = "TEIKAz5dnBhTiOjRkNsjMHehoZtsC3Ku";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-        audience: "https://dev-hiwm6xv136r7yzch.us.auth0.com/api/v2/", // Your API
-        scope: "read:events write:events offline_access",
-      }}
-    >
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>
-);
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+          audience: "https://dev-hiwm6xv136r7yzch.us.auth0.com/api/v2", // Your API
+          scope: "read:events write:events offline_access",
+        }}
+      >
+        <App />
+      </Auth0Provider>
+    </React.StrictMode>
+  );
